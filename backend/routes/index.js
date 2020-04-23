2f449ae6d2d645d644aac75fef05f120bd82382a
aundus <aundus@stevens.edu>
2020-04-23 19:44:58 -0400

    Add backend location APIs to get hospitals around zip code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const donorRoutes = require("./donors");
+const locationRoutes = require("./locations");
 // const hospitalRoutes = require("./hospitals");
 
 const constructorMethod = (app) => {
   app.use("/", router);
   app.use("/donors", donorRoutes);
+  app.use("/locations", locationRoutes);
   //   app.use("/hospitals", hospitalRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({
