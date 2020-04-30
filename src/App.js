11c9d5b5ba698da84ecebe2e045f8f5aecc24551
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2020-04-30 12:28:56 -0400

    created contact.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import signup from "./components/SignUp";
 import hospitals from "./components/Hospitals";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import contact from "./components/Contact"
 import { AuthProvider } from "./firebase/Auth";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -21,6 +22,7 @@ function App() {
             <Route path="/signin" exact component={signin} />
             <Route path="/sign-up" exact component={signup} />
             <Route path="/hospitals" exact component={hospitals} />
+            <Route path="/contact" exact component={contact} />
           </div>
         </div>
       </Router>
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  return <p>contact</p>;
+};
+
+export default Home;
