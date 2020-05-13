719015b0e09581f42c355254a066467d4362373c
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2020-05-13 15:18:05 -0400

    rectified css error

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,7 +1,38 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 
-function Faq() {
-  return <p>Frequently Asked Questions</p>;
-}
+const Faq = () => {
+  return (
+    <div class="container">
+      <div class="about">
+        <h2 class="heading">If I am a hospital/donor where do I sign up?</h2>
+        <p>
+        Click the donors page, and it will take you to a form. Fill in the 
+        information required in the fields, and you can see the number of hospitals 
+        in the nearby area once you have filled out the form.
+
+        </p>
+        <h2 class="heading">How do I contact you?</h2>
+        <p>
+        You can contact us by reaching out to the email under the contact us page.
+
+        </p>
+
+        <h2 class="heading">Can I see which hospitals require supplies before I choose to  donate to them?</h2>
+        <p>
+        Yes you can…. (I forgot what this part of the website is called, if you do remember can you add it in her)
+        </p>
+
+        <h2 class="heading">What are some items I can donate?</h2>
+        <p>
+        Preferred items include gloves and masks, hospitals will indicate which supplies they absolutley need once you sign up.
+        </p>
+        
+      </div>
+      <footer class="footer">
+        2020 &#169; Stevens Institute of Technology
+      </footer>
+    </div>
+  );
+};
 
 export default Faq;
