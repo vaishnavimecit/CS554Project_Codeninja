ed9bf9ed9c03693972891914ffb9e704d300341b
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2020-05-13 16:32:09 -0400

    FAQ page

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,31 +1,34 @@
 import React from "react";
+import "../App.css";
 
 const Faq = () => {
   return (
     <div class="container">
-      <div class="about">
-        <h2 class="heading">If I am a hospital/donor where do I sign up?</h2>
-        <p>
+      <div >
+        <div>
+          <br></br>
+        <h2 class="question">  If I am a hospital/donor where do I sign up?</h2>
+        </div>
+        <h4 className="answer">
         Click the donors page, and it will take you to a form. Fill in the 
         information required in the fields, and you can see the number of hospitals 
         in the nearby area once you have filled out the form.
-
-        </p>
-        <h2 class="heading">How do I contact you?</h2>
-        <p>
+        </h4>
+        <br></br>
+        <h2 class="question">How do I contact you?</h2>
+        <h4 className="answer">
         You can contact us by reaching out to the email under the contact us page.
-
-        </p>
-
-        <h2 class="heading">Can I see which hospitals require supplies before I choose to  donate to them?</h2>
-        <p>
-        Yes you can…. (I forgot what this part of the website is called, if you do remember can you add it in her)
-        </p>
-
-        <h2 class="heading">What are some items I can donate?</h2>
-        <p>
+        </h4>
+        <br></br>
+        <h2 class="question">Can I see which hospitals require supplies before I choose to  donate to them?</h2>
+        <h4 className="answer">
+        Yes you can click on Hospitals tab on home page and search the hospital you want
+        </h4>
+        <br></br>
+        <h2 class="question">What are some items I can donate?</h2>
+        <h4 className="answer">
         Preferred items include gloves and masks, hospitals will indicate which supplies they absolutley need once you sign up.
-        </p>
+        </h4>
         
       </div>
       <footer class="footer">
