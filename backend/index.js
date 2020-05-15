9ef08099e82ef7f994ef4a1d1087839126572cad
aundus <aundus@stevens.edu>
2020-05-15 13:34:24 -0400

    add origin validation

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,19 @@
 const express = require("express");
 const app = express();
-const session = require("express-session");
 const uuid = require("uuid");
 
 const configRoutes = require("./routes");
 
-// const determineUserRole = (req, res, next) => {
-//   if (req.session.user) {
-//     req.session.user.sessionId = req.session.id;
-//     delete req.session.user.password;
-//   }
-//   next();
-// };
-
-// app.use(
-//   session({
-//     name: "AuthCookie",
-//     secret: uuid(),
-//     saveUninitialized: true,
-//     resave: false,
-//     cookie: {
-//       expires: 3000000,
-//     },
-//   })
-// );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
-// app.use(determineUserRole);
 
 configRoutes(app);
 
