55dad2ef9c45dc1d196233493590c8d01fc2d23f
sushilsaladi <saladi.sushil@gmail.com>
2020-05-17 13:17:02 -0400

    Update AssociateHospital.js

--- src/components/AssociateHospital.js
+++ src/components/AssociateHospital.js
@@ -1,95 +1,71 @@
-import React, { useContext, useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
-import Spinner from "react-bootstrap/Spinner";
-import Alert from "react-bootstrap/Alert";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+const hospitalData = require("../data").hospitals;
+const express = require("express");
+const router = express.Router();
 
-import axios from "axios";
-import { AuthContext } from "../firebase/Auth";
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const hospital = await hospitalData.getById(id);
+    res.json(hospital);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
-function AssociateHospital(props) {
-  const [message, setMessage] = useState(undefined);
-  const [hospitals, setHospitals] = useState(undefined);
-  const [loaded, setLoaded] = useState(false);
-  const [profile, setProfile] = useState(undefined);
-  const { currentUser } = useContext(AuthContext);
-  let content = null;
-  useEffect(() => {
-    async function getData() {
-      console.log(props.match.params.set_gid);
-      associate(props.match.params.set_gid);
+router.post("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const g_id = req.query.g_id;
+    if (!id || !g_id) {
+      throw new Error("Missing required prameters id/g_id");
     }
-    getData();
-  }, []);
-
-  function generateAlert(message, variant) {
-    return <Alert variant={variant}>{message}</Alert>;
+    const hospital = await hospitalData.updateGoogleId(id, g_id);
+    const redisKey =
+      "geo:" +
+      String(hospital.location.latitude) +
+      "," +
+      String(hospital.location.latitude);
+    redisClient.delAsync(redisKey);
+    res.json(hospital);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
   }
+});
 
-  async function associate(g_id) {
-    try {
-      //${profile.id}?g_id=${g_id}`
-      await axios
-        .get(`http://localhost:3001/hospitals/getHospital/${g_id}`)
-        .then((data) => {
-          setHospitals(data);
-          console.log(hospitals);
-        });
-      console.log(hospitals);
-      setLoaded(true);
-    } catch (e) {
-      if (typeof e === "string") {
-        setMessage(generateAlert(e, "danger"));
-      } else {
-        setMessage(
-          generateAlert(e.message ? e.message : JSON.stringify(e), "danger")
-        );
-      }
+router.get("/getHospital/:set_gid", async (req, res) => {
+  try {
+    const gid = req.params.set_gid;
+    if (typeof gid !== "string") {
+      throw new Error("Invalid google id provided");
     }
+    const hospital = await hospitalData.getByGoogleId(gid);
+    res.json(hospital);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
   }
+});
 
-  function getContent() {
-    const items = [];
-    console.log(hospitals);
-    items.push(
-      <Card key={hospitals.data.google_id}>
-        <Card.Body>
-          <Card.Title>{hospitals.data.name}</Card.Title>
-          <Card.Text>{hospitals.data.city}</Card.Text>
-          <Card.Text>{hospitals.data.state}</Card.Text>
-          <Card.Text>{hospitals.data.phone}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
-
-    return <div>{items}</div>;
+router.get("/email/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const hospital = await hospitalData.getByEmail(email);
+    res.json(hospital);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
   }
+});
 
-  function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+router.post("/", async (req, res) => {
+  try {
+    const hospital = await hospitalData.createNew(req.body);
+    res.json(hospital);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
   }
+});
 
-  if (hospitals) {
-    content = getContent();
-  } else {
-    content = (
-      <div>
-        <h2>Just One More Step!</h2>
-        <Spinner className="center" animation="border" variant="primary" />
-        <br></br>
-        <small className="center">Loading Data On Your Region...</small>
-      </div>
-    );
-  }
-  return (
-    <div>
-      {content}
-      <footer class="footer">
-        2020 &#169; Stevens Institute of Technology
-      </footer>
-    </div>
-  );
-}
-
-export default AssociateHospital;
+module.exports = router;
